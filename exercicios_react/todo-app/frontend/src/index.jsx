5d91974bec9a579ad5a5a923a1f8c202d8a561b8
victormendeslc <victormendeslyma@gmail.com>
2017-11-23 16:44:29 -0200

    TodoApp

---
+++ exercicios_react/todo-app/frontend/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+import App from './main/app'
+import reducers from './main/reducers'
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
