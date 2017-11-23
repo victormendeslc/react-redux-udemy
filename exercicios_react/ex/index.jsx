1d0a0b1fe625e13d0438b5db7d98142da9abb810
victormendeslc <victormendeslyma@gmail.com>
2017-11-23 16:41:28 -0200

    Integração Redux
    Integrando com o react-redux

---
+++ exercicios_react/ex/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Field from './field'
+
+ReactDOM.render(
+    <Field initialValue='Teste' />
+    ,document.getElementById('app')
+);
